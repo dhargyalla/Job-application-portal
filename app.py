986c76582b7834996a63642b7b59e1b6d84db353
Tsering Dhargyal <dhargyalofficial@gmail.com>
2025-07-23 14:46:32 +0100

    add dynamic data and jsonify data

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from flask_bootstrap import Bootstrap5
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
+
+jobs = [
+    {
+        'id': 1,
+        'title': 'Data Engineer',
+        'location': 'Sheffield',
+        'Salary': '$60K - $100K',
+        'Requirements': ['React','Javascript','Python','Maths']
+
+    },
+    {
+        'id': 2,
+        'title': 'Data Analysis',
+        'location': 'London',
+        'Salary': '$50K - $140K',
+        'Requirements': ['React','Javascript','Python','Maths']
+    },
+    {
+        'id': 3,
+        'title': 'Project manager',
+        'location': 'Leeds',
+        'Salary': '$30K - $130K',
+        'Requirements': ['SPSS','Excel','Certification','Maths']
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Manchester',
+        'Salary': '$50K - $200K',
+        'Requirements': ['React','Javascript','Python','Maths']
+    },
+]
+
 
 @app.route('/')
 def home():
@@ -11,6 +44,14 @@ def home():
 @app.route('/search')
 def search():
     return render_template('search.html')
+
+@app.route('/jobopening')
+def applyjob():
+    return render_template('jobopening.html',jobs = jobs, company_name = 'technologia')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run()
